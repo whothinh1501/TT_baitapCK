5d9d498caf64f3048de17650745bf34240e57e16
Huu_Thinh <huuthinh494@gmail.com>
2023-04-25 22:48:38 +0700

    add file cart.html, login.html

--- script.js
+++ script.js
@@ -9,13 +9,13 @@ document.getElementById("stores").addEventListener("click", function() {
   window.location.href = "https://kfcvietnam.com.vn/he-thong-nha-hang-kfc";
 });
 
-document.getElementById("myButton").addEventListener("click", function() {
-  window.location.href = "https://kfcvietnam.com.vn/store-selection";
-});
-
-// document.getElementById("sign_in").addEventListener("click", function() {
-//   window.location.href = "https://kfcvietnam.com.vn/account/login?returnUrl=%2F";
+// document.getElementById("myButton").addEventListener("click", function() {
+//   window.location.href = "https://kfcvietnam.com.vn/store-selection";
 // });
+
+document.getElementById("sign_in").addEventListener("click", function() {
+  window.location.href = "cart.html";
+});
 
 function goToNewPage() {
   window.location.href = 'deliverypage.html';
